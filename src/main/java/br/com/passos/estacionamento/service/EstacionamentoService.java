2f5058c0af3016c4e48bcc545592f710b15ed6fa
Paulo César dos Passos Pereira <paulo.passos1@gmail.com>
2024-03-22 09:04:13 -0300

    atualizando o status de ativa da vaga
    A vaga fica ativa logo após ser vinculada com o veiculo

--- src/main/java/br/com/passos/estacionamento/service/EstacionamentoService.java
+++ src/main/java/br/com/passos/estacionamento/service/EstacionamentoService.java
@@ -1,10 +1,14 @@
 package br.com.passos.estacionamento.service;
 
+import br.com.passos.estacionamento.domain.Vaga;
+import br.com.passos.estacionamento.repository.VagaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.passos.estacionamento.domain.Estacionamento;
 import br.com.passos.estacionamento.repository.EstacionamentoRepository;
 import br.com.passos.estacionamento.service.useCase.DataValidador;
+
+import java.util.Optional;
 
 @Service
 public class EstacionamentoService{
@@ -13,11 +17,22 @@ public class EstacionamentoService{
     private EstacionamentoRepository estacionamentoRepository;
 
     @Autowired
+    private VagaRepository vagaRepository;
+
+    @Autowired
     private DataValidador dataValidador;
 
     public void adicionar(Estacionamento estacionamento){
         dataValidador.isEndDateAfterStartDate(estacionamento.getDataEntrada(), estacionamento.getDataSaida());
         estacionamentoRepository.save(estacionamento);
+        vagaAtiva(estacionamento.getId());
+    }
+
+    private void vagaAtiva(Long id){
+        Optional<Vaga> vaga = vagaRepository.findById(id);
+        Vaga vagaAtiva = vaga.get();
+        vagaAtiva.setAtiva(true);
+        vagaRepository.save(vagaAtiva);
     }
 
 }
